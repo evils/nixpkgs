52e19274bc41f13e4d1aef2f97e9e99fe7cc8e88
Evils <evils.devils@protonmail.com>
2021-05-12 04:58:00 +0200

    clar-oui: init with clarissa.src

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3530,6 +3530,8 @@ with pkgs;
 
   clarissa = callPackage ../tools/networking/clarissa { };
 
+  clar-oui = callPackage ../tools/networking/clarissa/oui.nix { };
+
   cloudfox = callPackage ../tools/security/cloudfox { };
 
   cloudhunter = callPackage ../tools/security/cloudhunter { };
