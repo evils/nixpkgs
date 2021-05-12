c5367feb0f743a4c6e8a137ea4b073d348ed36c3
Evils <evils.devils@protonmail.com>
2021-05-12 04:58:00 +0200

    clar-oui: init with clarissa.src

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1455,6 +1455,7 @@ with pkgs;
   fedora-backgrounds = recurseIntoAttrs (callPackage ../data/misc/fedora-backgrounds { });
 
   clarissa = callPackage ../tools/networking/clarissa { };
+  clar-oui = callPackage ../tools/networking/clarissa/oui.nix { };
 
   coconut = with python312Packages; toPythonApplication coconut;
 
