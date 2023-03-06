32c91427d81fef59bf17cca919ca4338c6296eeb
Evils <evils.devils@protonmail.com>
2023-03-06 18:20:16 +0100

    ptouch-print: bump to latest master

--- pkgs/misc/ptouch-print/default.nix
+++ pkgs/misc/ptouch-print/default.nix
@@ -1,22 +1,39 @@
 { lib, stdenv
 , fetchgit
-, autoreconfHook
+, cmake
 , gd
 , libusb1
+, gettext
+, git
+, pkg-config
 }:
 
 stdenv.mkDerivation rec {
   pname = "ptouch-print";
-  version = "1.4.3";
+  version = "v1.5-9-g6b82cd6";
 
   src = fetchgit {
-    url = "https://mockmoon-cybernetics.ch/cgi/cgit/linux/ptouch-print.git";
-    rev = "v${version}";
-    sha256 = "0i57asg2hj1nfwy5lcb0vhrpvb9dqfhf81vh4i929h1kiqhlw2hx";
+    url = "https://git.familie-radermacher.ch/linux/ptouch-print.git";
+    rev = "6b82cd6166745505849004cd3ef4a21cfb2336e6";
+    hash = "sha256-stg8lm06wcBCE6fL0ehwepcHuFoT0lFHLKPL3QNT6lg=";
   };
 
+  preConfigure = ''
+    substituteInPlace CMakeLists.txt --replace "/usr" "${placeholder "out"}"
+  '';
+
+  # doesn't work, hence the above SubstituteInPlace
+  /*
+  cmakeFlags = [
+    "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
+  ];
+  */
+
   nativeBuildInputs = [
-    autoreconfHook
+    cmake
+    gettext
+    git
+    pkg-config
   ];
 
   buildInputs = [
@@ -26,9 +43,9 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Command line tool to print labels on Brother P-Touch printers on Linux";
-    license = licenses.gpl3Plus;
-    homepage = "https://mockmoon-cybernetics.ch/computer/p-touch2430pc/";
-    maintainers = with maintainers; [ shamilton ];
+    license = licenses.gpl3Only;
+    homepage = "https://dominic.familie-radermacher.ch/projekte/ptouch-print/";
+    maintainers = with maintainers; [ shamilton evils ];
     platforms = platforms.linux;
   };
 }
